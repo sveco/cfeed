b0b3c69e6f27ca90eb03f788a36833f1d2f429f0
Janko <ultrabrutalac@gmail.com>
2017-09-14 13:23:41 +0200

    Better handling of line length. I hope.

--- CRR/StringExtensions.cs
+++ CRR/StringExtensions.cs
@@ -8,26 +8,28 @@ using System.Threading.Tasks;
 
 namespace CRR
 {
-    public static class StringExtensions
-    {
-        public static string SanitizeFileName(this string filename)
-        {
-            string regexSearch = new string(Path.GetInvalidFileNameChars());
-            Regex r = new Regex(string.Format("[{0}]", Regex.Escape(regexSearch)));
-            return r.Replace(filename, "");
-        }
+	public static class StringExtensions
+	{
+		public static string SanitizeFileName(this string filename)
+		{
+			string regexSearch = new string(Path.GetInvalidFileNameChars());
+			Regex r = new Regex(string.Format("[{0}]", Regex.Escape(regexSearch)));
+			return r.Replace(filename, "");
+		}
 
-        public static string SanitizePath(this string path)
-        {
-            string regexSearch = new string(Path.GetInvalidPathChars());
-            Regex r = new Regex(string.Format("[{0}]", Regex.Escape(regexSearch)));
-            return r.Replace(path, "");
-        }
+		public static string SanitizePath(this string path)
+		{
+			string regexSearch = new string(Path.GetInvalidPathChars());
+			Regex r = new Regex(string.Format("[{0}]", Regex.Escape(regexSearch)));
+			return r.Replace(path, "");
+		}
 
-        public static int VisibleLength(this string str) {
-            var stripedAnsi = Regex.Replace(str, @"\e\[(\d+;)*(\d+)?[ABCDHJKfmsu]", "");
-            var stripedControl = Regex.Replace(stripedAnsi, @"[^\x20-\x7F]", "");
-            return stripedControl.Length;
-        }
-    }
+		public static int VisibleLength(this string str)
+		{
+			//var stripedAnsi = Regex.Replace(str, @"\e\[(\d+;)*(\d+)?[ABCDHJKfmsu]", "");
+			var stripedAnsi = Regex.Replace(str, @"\x1b\[(\d+;)*(\d+)?[ABCDHJKfmsu]", "");
+			var stripedControl = Regex.Replace(stripedAnsi, @"[^\x20-\x7F]", "");
+			return stripedControl.Length;
+		}
+	}
 }
