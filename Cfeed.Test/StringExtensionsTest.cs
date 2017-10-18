1ef31f8651d2ecd65ed8350eb4bc80bb1a79d598
Sveco <ultrabrutalac@gmail.com>
2017-10-18 14:48:16 +0200

    Added SanitizePathTest.

--- Cfeed.Test/StringExtensionsTest.cs
+++ Cfeed.Test/StringExtensionsTest.cs
@@ -11,13 +11,26 @@ namespace Cfeed.Test
     [TestMethod]
     public void SanitizeFileNameTest()
     {
-      var invalidChars = new String(Path.GetInvalidPathChars());
+      var invalidChars = Path.GetInvalidFileNameChars();
 
-      var pathString = "Test" + invalidChars + ".txt";
+      var pathString = "Test" + new String(invalidChars) + ".txt";
       var sanitizedPath = StringExtensions.SanitizeFileName(pathString);
-      bool possiblePath = sanitizedPath.IndexOfAny(Path.GetInvalidPathChars()) == -1;
+      bool possiblePath = sanitizedPath.IndexOfAny(invalidChars) == -1;
 
       Assert.AreEqual<string>(sanitizedPath, "Test.txt");
+      Assert.IsTrue(possiblePath);
+    }
+
+    [TestMethod]
+    public void SanitizePathTest()
+    {
+      var invalidChars = Path.GetInvalidPathChars();
+
+      var pathString = "c:\\Test" + new String(invalidChars) + "\\";
+      var sanitizedPath = StringExtensions.SanitizePath(pathString);
+      bool possiblePath = sanitizedPath.IndexOfAny(Path.GetInvalidPathChars()) == -1;
+
+      Assert.AreEqual<string>(sanitizedPath, "c:\\Test\\");
       Assert.IsTrue(possiblePath);
     }
   }
